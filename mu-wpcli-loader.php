e38892bda426dc4af9fdc6279be5df98562864a0
Christopher McComas <mccomas.chris@gmail.com>
2025-03-13 20:09:33 -0400

    actually call our commands

--- mu-wpcli-loader.php
+++ mu-wpcli-loader.php
@@ -5,9 +5,10 @@
  * Author: Christopher McComas
  * Version: 1.0
  *
- * @package MU_WPCLI
+ * @package Marshall_WPCLI
  */
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once __DIR__ . '/mu-wpcli-commands/commands/class-sample-command.php';
+	require_once __DIR__ . '/mu-wpcli-commands/commands/class-get-urls-csv-command.php';
+	require_once __DIR__ . '/mu-wpcli-commands/commands/class-keyword-scanner-command.php';
 }
