eeff09f04a7caaff4e4a7718ea508b5a6e845deb
Christopher McComas <mccomas.chris@gmail.com>
2025-03-13 20:12:51 -0400

    wip

--- mu-wpcli.php
+++ mu-wpcli.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Plugin Name: Marshall WP-CLI
+ * Plugin URI: https://pantheon.io/
  * Description: Custom WP-CLI commands for Marshall University.
+ * Version: 1.0.0
  * Author: Christopher McComas
- * Version: 1.0
  *
  * @package Marshall_WPCLI
  */
